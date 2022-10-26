12e1e29cccb4cc82e541927f5d4dd944344da5c4
Justin Chu <developer@justinchu.com>
2022-10-25 23:39:24 -0400

    Add GasBot react with Giphy

--- src/Helpers/bot.ts
+++ src/Helpers/bot.ts
@@ -1,3 +1,8 @@
+import axios from 'axios';
+import random from 'random'
+import {filename} from '../constants';
+const config = require("../../"+filename);
+
 export function populateArray(value:string){
     let resArray = [];
     value.split(",").forEach(element=>{
@@ -6,3 +11,9 @@ export function populateArray(value:string){
     return resArray;
 }
 
+export async function fetch_giphy(query:string):Promise<string>{
+    let randomSeed = random.int(0,1000);
+    let url=`https://api.giphy.com/v1/gifs/search?api_key=${config.GIPHY_KEY}&q=${query}&limit=1&offset=${randomSeed}&rating=pg-13&lang=en`;
+    let res = await axios.get(url);
+    return await res.data.data[0].embed_url;
+}                                 
--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
-import {isTermUsed, isUser} from "./Helpers/discord";
+import {isGasBot, isTermUsed, isUser} from "./Helpers/discord";
 import {Client} from "discord.js";
+import {fetch_giphy} from './Helpers/bot';
 import {filename,emoji} from './constants';
 const config = require("../"+filename);
 
@@ -9,10 +10,22 @@ client.on('ready', () => {
      console.log("EmanBot is now online!");
 });
 
-client.on('message', msg => {    
+client.on('message',async msg => {
      if(isUser(msg.author.username,msg.author.discriminator)==true||
-     isTermUsed(msg.content) == true){
+     isTermUsed(msg.content) == true){         
           msg.react(emoji);
+     }
+
+     if(isGasBot(msg.author)){
+          let gif = "";
+          if(msg.embeds[0].title.includes("+")){
+               gif = await fetch_giphy("angry");
+          }else if(msg.embeds[0].title.includes("-")){
+               gif = await fetch_giphy("celebrate");
+          }else{
+               gif = await fetch_giphy("shrug");
+          }
+          msg.reply(gif);
      }
 });
 
